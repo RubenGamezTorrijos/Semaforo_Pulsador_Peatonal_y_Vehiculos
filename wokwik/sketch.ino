036cd1d8ef4b18dae32588b73d1faaf3907c4e10
Rubén Gámez Torrijos <19588354+RubenGamezTorrijos@users.noreply.github.com>
2024-06-05 00:12:39 +0200

    Create sketch.ino

---
+++ wokwik/sketch.ino
@@ -0,0 +1,13 @@
+extern "C" {
+  // put your setup code here, to run C:
+}
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
